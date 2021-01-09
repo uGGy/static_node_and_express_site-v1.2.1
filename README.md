9158d50d318c442522205e1504f98107cc182c6c
Alexis Vogelsperger <voo.alexis@gmail.com>
2021-01-09 17:04:49 +0100

    Files for Portofolio project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # static_node_and_express_site-v1.2
  6th project // Coding Portofolio
+
+Style changed: 
+
+- Fonts
+- background color profile 
+- 'Learn More' color 
+- 'Back' button color
+- Border Size projects images
--- app.js
+++ app.js
@@ -2,40 +2,62 @@ const express = require('express');
 const app = express(); 
 
 const data = require('./data.json');
-const { projects } = data; 
-console.log(projects);
+const { projects } = data;
+
 
 app.use('/static', express.static('public'));
 app.set( 'view engine', 'Pug');
 
 app.get('/', (req, res) => {
-    res.render('index', projects);
+    res.render('index', { project: data.projects });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', projects);
+    res.render('about', { projects: projects });
 });
 
 app.get('/project/:id', (req, res) => {
-    res.render('project', projects);
+        
+        const { id } = req.params;
+        const title = projects[id].project_Name;
+        const description = projects[id].description;
+        const technologies = projects[id].technologies;
+        const img = projects[id].image_urls;
+        const gitRepoPath = projects[id].github_link;
+        const gitLinkDemo = projects[id].live_link;
+
+        const templateData = { id, title, description, technologies, img, gitRepoPath, gitLinkDemo };
+
+    
+    res.render('project', templateData);
 });
 
 
-// Handling Errors 
+// ** Handling Errors 
+
+//404 Error
 
 app.use((req, res, next) => {
-    const err404 = new Error('It seems like this page does not exist');
-    const err500 = new Error('Outch, error 500! There is a server problem');
+    const err = new Error();
+    err.status = 404; 
+    err.message = 'Looks like this page does not exist.'
+    res.status(404).render('page-not-found');
+});
 
-   if  (err.status = 404) {
-    next(err404);
-   };
 
-   if (err.stauts = 500) {
-       next(err500);
-   };
+// Globabl Error
+
+app.use((err, req, res, next) => {
+
+    if (err.status === 404) {
+        res.status(404).render('page-not-found', { err });
+    } else {
+        res.status(err.status || 500).render('error', { err });
+        
+}
 
 });
+
 
 // Local Sever Host
 app.listen(3000);
